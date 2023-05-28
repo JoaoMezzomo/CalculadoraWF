d03c8d4933cb46e1bb34fb4a0e040c8f909f1548
João Mezzomo <joaowll83@gmail.com>
2023-05-28 17:35:15 -0300

    Ajustes para utilizar .net5

--- CalculadorWF/Program.cs
+++ CalculadorWF/Program.cs
@@ -14,6 +14,7 @@ namespace CalculadorWF
         [STAThread]
         static void Main()
         {
+			Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
